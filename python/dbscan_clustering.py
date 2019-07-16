8756e821fa5e7b00c878b6dd6b6560cf5bd6071f
Ayush Suresh <ayushboss@Ayushs-MacBook-Air-2.local>
2019-07-16 11:33:57 -0500

    finished density score calculation, need to determine proper eps value

---
+++ python/dbscan_clustering.py
@@ -0,0 +1,30 @@
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score as silscore
+from sklearn.metrics import pairwise_distances as pair_dist
+import pandas as pd
+import numpy as np
+import csv
+
+#reading in and parsing the data
+data = pd.read_csv('cluster_data.csv', error_bad_lines=False, engine="python") 
+print(data)
+
+#maybe?? : converting the returned value from read_csv to a nested list to pass into dbscan
+
+#applying dbscan to the data
+np_data = np.asarray(data)
+print(np_data)
+
+#NEED TO OPTIMIZE THE EPS VALUE IN ORDER TO ENSURE THAT WE GET A GOOD AMOUNT OF CLUSTERSS
+#TALK TO DR.TAMIR ABOUT IT
+
+clustering = DBSCAN(eps=0.001, min_samples=2).fit(np_data) 
+print(clustering.labels_)
+print ("Size of clustering: " + str(len(clustering.labels_)))
+print(clustering)
+
+#assess the quality of the clustering scheme by using the sillhouette_score
+#this sillhouette score basically tells us about the density of the clustering
+
+overall_clustering_score = silscore(np_data, clustering.labels_, "euclidean")
+print(overall_clustering_score)
--- python/pca_clustering.py
+++ python/pca_clustering.py
@@ -14,9 +14,14 @@ d = 'd'
 
 z_scaler = StandardScaler()
 
-data = pd.read_csv('cluster_data.csv', error_bad_lines=False, engine="python")
+data = pd.read_csv('cluster_data.csv', error_bad_lines=False, engine="python") #reads and parses the data
 print(data)
 print(data.head())
+
+'''
+the following code generates and fits
+our data to a PCA model
+'''
 
 scaled_data = preprocessing.scale(data.T)
 pca = PCA()
@@ -24,13 +29,26 @@ pca.fit(scaled_data)
 pca_data = pca.transform(scaled_data)
 
 per_var = np.round(pca.explained_variance_ratio_ * 100, decimals = 1)
-labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)]
+labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)] #generates labels for the plots based on the number of principal categories that were calculated
+
+'''
+the following plot informs us
+of the contributions of each 
+of the principal components
+'''
 
 plt.bar(x=range(1, len(per_var) + 1), height = per_var, tick_label = labels)
 plt.ylabel('Percentage of Expected Variance')
 plt.xlabel('Principal Component')
 plt.title("Scree Plot")
 plt.show()
+
+'''
+the following plot informs us 
+of which features are correlated
+and how much they contribute
+to each of the principal components
+'''
 
 pca_df = pd.DataFrame(pca_data, index=["Entropy", "Compression"], columns=labels)
 plt.scatter(pca_df.PC1, pca_df.PC2)
@@ -42,6 +60,12 @@ for sample in pca_df.index:
 	plt.annotate(sample, (pca_df.PC1.loc[sample], pca_df.PC2.loc[sample]))
 
 plt.show()
+
+'''
+the following section informs us 
+about which samples contribute 
+the most to the pca measurement
+'''
 
 loading_scores = pd.Series(pca.components_[0])
 sorted_loading_scores = loading_scores.abs().sort_values(ascending=False)
