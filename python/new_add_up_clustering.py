53236922d8556a488f066421ef3e6c446f759e18
Ayush Suresh <byteslayer31037@gmail.com>
2019-10-21 12:47:14 +0200

    obtained p-value heatmap, but need to clean up code

---
+++ python/new_add_up_clustering.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import csv
+import seaborn as sns
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
+from scipy import stats
+import pingouin as pg
+import sys
+import os
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#from isodata import find_clusters
+
+data = pd.read_csv('../cluster_data_philox.csv', error_bad_lines=False, engine="python") #reads and parses the data
+
+print(type(data))
+
+n_clusters = 5
+
+labels = ["Entropy", "Compression Ratio", "Monobit", "Frequency Within Block", "Runs",
+            "Longest Runs in Ones", "Binary Matrix Rank", "DFT", 
+            "Non-Overlapping Template", "Overlapping Template", "Maurer's Universal", 
+            "Linear Complexity"]
+correlating_labels=["Entropy", "Compression Ratio", "Monobit", "Frequency Within Block", "Runs",
+            "Longest Runs in Ones", "Binary Matrix Rank", "DFT", 
+            "Non-Overlapping Template", "Overlapping Template", "Maurer's Universal", 
+            "Linear Complexity"]
+df = pd.DataFrame(data)
+
+columns = list(df)
+features = []
+for column in df:
+	epiclist = df[column] # epiclist = values for a certain feature for all iterations
+	features.append(epiclist)
+
+#generates a heatmap that we can use to find strongly correlated values
+plt.figure(figsize=(12,10))
+cor = df.corr(method='pearson')
+
+print("checkpoint")
+print(pg.pairwise_corr(df).sort_values(by=['p-unc'])[['X', 'Y', 'n', 'r', 'p-unc']])
+
+print("checkpoint 2")
+print(cor)
+
+pingouin_raw_corr = pg.pairwise_corr(df).sort_values(by=['p-unc'])[['X', 'Y', 'n', 'r', 'p-unc']]
+
+pingouin_corr = pd.DataFrame()
+
+for x in df.columns:
+    for y in df.columns:
+        corr = stats.pearsonr(df[x], df[y])
+        pingouin_corr.loc[x,y] = corr[1]
+print("lets go")
+print(pingouin_corr)
+
+
+sns.heatmap(pingouin_corr, annot=True, cmap=plt.cm.Reds)
+plt.show()
+
+df.drop(df.index[0])
+
+correlations = {}
+
+for i in range(0, len(labels)):
+	label = labels[i]
+	cor_target = abs(cor[label])
+	relevant_features = cor_target[cor_target>0.05]
+	correlations[correlating_labels[i]] = relevant_features
+	
+#deleting highly correlated values in order to get only one value from each cluster
+
+for l in range(0,len(features)):
+	for m in range(l, len(features)):
+		if (m >= len(features)):
+			continue
+
+		i = correlating_labels[l]
+		j = features[m]
+
+		#correlations[i].keys() gives me the names of the correlating features
+
+		if (np.logical_and(l != m,correlating_labels[m] in correlations[i].keys())):
+			features.pop(m)
+			m-=1
+
+#attempt add up	
+
+best_features_per_num_of_features = []
+best_coeff_per_num_of_features = []
+
+features_already_included = []
+
+features_already_included_hashing = [False] * len(features)
+
+for r in range(0, len(features)): # r represents the number of features we are adding
+	best_features = []
+
+	for i in range(0,len(features_already_included_hashing)):
+		if (features_already_included_hashing[i] == True):
+			best_features.append(features[i])
+
+	best_coeff = 0
+	best_coeff_idx = 0
+	for d in range(0, len(features)):
+		if (features_already_included_hashing[d] == True):
+			continue
+
+		feature = features[d].copy() # returns the exact feature that we are looking at rn
+		best_features.append(features[d])
+		clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+		
+		#print (features)
+
+		best_features_df = pd.DataFrame(best_features)
+		best_features_df_trans = best_features_df.transpose() #transpose to get everything in terms of trial number rather than test
+
+		cluster_labels = clusterer.fit_predict(best_features_df_trans)
+		silhouette_avg = silhouette_score(best_features_df_trans, cluster_labels)
+		if (silhouette_avg > best_coeff):
+			best_coeff_idx = d
+			best_coeff = silhouette_avg
+		print("Sillhouette score for feature " + str(d) + " is " + str(silhouette_avg))
+		del best_features[-1]
+
+	best_features.append(features[best_coeff_idx])
+	features_already_included.append(best_coeff_idx)
+	features_already_included_hashing[best_coeff_idx] = True
+	best_features_per_num_of_features.append(best_features)
+	best_coeff_per_num_of_features.append(best_coeff)
+
+for i in range(0, len(best_features_per_num_of_features)):
+	print("_____________________ Best features for " + str(i + 1) + ":  _____________________")
+	for x in range(0, len(best_features_per_num_of_features[i])):
+		print(best_features_per_num_of_features[i][x].name)
+
+# centers, labels = find_clusters(B = features, n_clusters = 2, rseed = 2, iterations = 10)
+# features_np = np.array(features)
+
+# plt.scatter(features_np[:, 0], features_np[:, 1], c=labels,
+#            s=50, cmap='viridis')
+# plt.show()
+# #best_coeff_per_num_of_features indicates the silhouette coefficient 
+
+# make sure with professor that our add up algorithm is actually not ass lmao
