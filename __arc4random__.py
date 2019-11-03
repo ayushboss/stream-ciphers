829ac6cdbdc7243fad3d273835e697c0b37176af
Ayush Suresh <byteslayer31037@gmail.com>
2019-11-03 02:01:08 +0100

    finished acquiring PCG64 and Mersenne Twister data

--- __arc4random__.py
+++ __arc4random__.py
@@ -1,5 +1,6 @@
 import sp800_22_tests
 import arc4random
+import time
 
 instance_amnt = int(input("# of instances: "))
 bits_per_instance = int(input("# of bits per instance: "))
@@ -8,6 +9,11 @@ sp800_22_tests.append_header("cluster_data_arc4random.csv")
 
 for s in range(instance_amnt):
 	print("----------------------  Iteration " + str(s) + "  ----------------------")
-	bits = arc4random.randsample(0,1,bits_per_instance)
-	print("check")
+	bits=[]
+	start_time = time.time()
+	for x in range(100):	
+		bits.append(arc4random.randsample(0,1,int(bits_per_instance/100)))
+		print("Generating Bits: " + str(x))
+	end = time.time()
+	print("Duration: " + str(end-start_time))
 	sp800_22_tests.test_func(bits, "cluster_data_arc4random.csv")
