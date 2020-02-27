8cbb123fea93512806cc357bdd12421cf659ea18
Ayush Suresh <byteslayer31037@gmail.com>
2020-02-27 21:39:37 +0100

    finished compression stuff

--- compress_bin_files.py
+++ compress_bin_files.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
+import csv
+
 def compress(csvname):
     directory = os.fsencode("bin_files")
 
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith(".bin"): 
-            sCompressFiles = subprocess.check_call("7z a " + filename[:4] + " " + filename, shell = True)
+            print("test: " + filename[:-4] + ".7z & " + filename)
+            sCompressFiles = subprocess.check_call("cd bin_files; 7z a " + filename[:-4] + ".7z " + filename + "; cd ..", shell = True)
         continue
 
     iterationValue = -1
@@ -16,9 +19,29 @@ def compress(csvname):
 
     idx = 1
 
-    while idx != iterationvalue + 1:
-        normalFileSize = os.path.getsize("bin_files/bin_list_" + str(idx) + ".bin")
-        compressedFileSize = os.path.getsize("bin_files/bin_list_" + str(idx) + ".7z")
+    print("Iteration Value: " + iterationValue)
+
+    cwd = os.open("bin_files", os.O_RDONLY)  # Get the current working directory (cwd)
+    files = os.listdir(cwd)  # Get all the files in that directory
+    print("Files in %r: %s" % (cwd, files))
+
+    while idx != int(iterationValue) + 1:
+        # normalFileSize = os.path.getsize(os.path.join("bin_files/", "bin_list_" + iterationValue + ".bin"))
+        # file_path = os.path.join("bin_files", "bin_list_" + str(idx) + ".bin")
+        file_path=open("/Users/ayushboss/Desktop/Coding/Python/hsmc-stream-cipher-2019-git/11-3-19-2:03/stream-ciphers/bin_files/bit_list_" + str(idx) + ".bin")
+
+        compressed_path = os.path.join("bin_files", "bin_list_" + str(idx) + ".7z")
+        compressed_path = compressed_path.strip()
+
+        
+        compressed_file_path = open("/Users/ayushboss/Desktop/Coding/Python/hsmc-stream-cipher-2019-git/11-3-19-2:03/stream-ciphers/bin_files/bit_list_" + str(idx) + ".7z")
+
+        file_path.seek(0, os.SEEK_END)
+        compressed_file_path.seek(0, os.SEEK_END)
+
+        normalFileSize = file_path.tell()
+        compressedFileSize = compressed_file_path.tell()
+        print ("Normal, Compressed: " + str(normalFileSize) + " " + str(compressedFileSize))
         ratio = compressedFileSize/normalFileSize
         row = [ratio]
         with open(csvname, "a") as csvfile:
